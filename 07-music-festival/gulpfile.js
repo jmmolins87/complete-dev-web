1f29127a863241342f3f47ef3871015ce01fdf47
Juanma Molins Cortés <juan.maria.molins@accenture.com>
2024-08-30 13:55:04 +0200

    fix project fest

--- 07-music-festival/gulpfile.js
+++ 07-music-festival/gulpfile.js
@@ -1,21 +1,34 @@
 
 
 
-import { src, dest, watch } from 'gulp';
+import { 
+    src, 
+    dest, 
+    watch, 
+    series 
+} from 'gulp';
 import * as dartSass from 'sass';
 import gulpSass from 'gulp-sass';
 
 const sass = gulpSass(dartSass);
 
-export function css(done) {
+export function js(done) {
+    src('src/js/app.js', {sourcemaps: true})
+        .pipe(dest('scripts', {sourcemaps: '.'}));
+    done();
+}
 
-    src('src/scss/main.scss')
+export function css(done) {
+    src('src/scss/main.scss', {sourcemaps: true})
         .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-        .pipe(dest('styles'));
+        .pipe(dest('styles', {sourcemaps: '.'}));
         
     done();
 }
 
 export function dev() {
     watch('src/scss/**/*.scss', css);
+    watch('src/js/**/*.js', js);
 }
+
+export default series(js, css, dev);
---
+++ 07-music-festival/scripts/app.js
@@ -0,0 +1,112 @@
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    fixNavigation();
+    createGallery();
+    highlightLink();
+    scrollNav();
+});
+
+function fixNavigation() {
+    const header = document.querySelector('.wrap-header');
+    const fest = document.querySelector('.wrap-festival');
+
+    window.addEventListener('scroll', function() {
+        if(fest.getBoundingClientRect().bottom < 1) {
+            header.classList.add('fixed');
+        } else {
+            header.classList.remove('fixed');
+        }
+    });
+}
+
+function createGallery() {
+    const gallery = document.querySelector('.wrap-gallery-imgs');
+    const lengthImgs = 16;
+
+    for(let i = 1; i <= lengthImgs; i++) {
+        const image = document.createElement('IMG');
+        image.src = `src/assets/img/gallery/full/${i}.jpg`;
+        image.alt = 'Imagen de la Galería';
+        image.title = 'Imagen de la Galería';
+
+        // Event Handler
+        image.onclick = function() {
+            showModalImage(i);
+        };
+
+        gallery.appendChild(image);
+    }
+}
+
+function showModalImage(i) {
+    const image = document.createElement('IMG');
+    image.src = `src/assets/img/gallery/full/${i}.jpg`;
+    image.alt = 'Imagen de la Galería';
+    image.title = 'Imagen de la Galería';
+
+    // Generar modal
+    const modal = document.createElement('DIV');
+    modal.classList.add('wrap-modal');
+    modal.onclick = closeModal;
+    modal.appendChild(image);
+    // Botón cerrar Modal
+    const btnClose = document.createElement('BUTTON');
+    btnClose.textContent = 'X';
+    btnClose.classList.add('btn-close');
+    btnClose.onclick = closeModal;
+    modal.appendChild(btnClose);
+    // Agregar al HTML
+    const body = document.querySelector('body');
+    body.classList.add('no-scroll');
+    body.appendChild(modal);
+    console.log(modal);
+}
+
+function closeModal() {
+    const modal = document.querySelector('.wrap-modal');
+    const body = document.querySelector('body');
+    body.classList.remove('no-scroll');
+    modal.classList.add('fade-out');
+    setTimeout(() => {
+        modal?.remove();
+    }, 500);
+}
+
+function highlightLink() {
+    document.addEventListener('scroll', function() {
+        const sections = document.querySelectorAll('section');
+        const navLinks = document.querySelectorAll('.header-content__navbar .header-content__navbar--link');
+        let currentSection = '';
+        sections.forEach( section => {
+            const sectionTop = section.offsetTop;
+            const sectionHight = section.clientHeight;
+            if(window.scrollY >= (sectionTop - sectionHight / 3)) {
+                currentSection = section.id;
+            }
+            navLinks.forEach(link => {
+                if(link.getAttribute('href') === `#${currentSection}`) {
+                    link.classList.add('active');
+                } else {
+                    link.classList.remove('active');
+                }
+            })
+        })
+    });
+}
+
+function scrollNav() {
+    const navLinks = document.querySelectorAll('.header-content__navbar .header-content__navbar--link');
+    navLinks.forEach(link => {
+        link.addEventListener('click', e => {
+            e.preventDefault();
+            const sectionScroll = e.target.getAttribute('href');
+            const section = document.querySelector(sectionScroll);
+
+            section.scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    })
+}//# sourceMappingURL=app.js.map
